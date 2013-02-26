103bb1f46d1a476d5c429b93b0300fcb83ee17dd
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-27 00:54:25 +0100

    Ajout d'un début de tuto pour prendre la suite de celui de l'installeur

---
+++ app/local/site/view/index/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=<?php echo __HTML_ENCODING__; ?>">
+    <title><?php echo $data['titre']; ?></title>
+    <link rel="stylesheet" href="<?php echo __WWW_ROOT__; ?>/install/style.css" type="text/css" media="screen" charset="<?php echo __HTML_ENCODING__; ?>" />
+</head>
+<body>
+    <div id="wrapper">
+        <div id="main">
