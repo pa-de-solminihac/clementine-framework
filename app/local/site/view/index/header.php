590b1e0b29638af13294e3b59b5ca69e5a982d66
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-28 03:26:03 +0100

    Feuille de style en dur pour l'autonomie du tuto

--- app/local/site/view/index/header.php
+++ app/local/site/view/index/header.php
@@ -3,7 +3,103 @@
 <head>
     <meta http-equiv="content-type" content="text/html; charset=<?php echo __HTML_ENCODING__; ?>">
     <title><?php echo $data['titre']; ?></title>
-    <link rel="stylesheet" href="<?php echo __WWW_ROOT__; ?>/install/style.css" type="text/css" media="screen" charset="<?php echo __HTML_ENCODING__; ?>" />
+    <style type="text/css" media="screen">
+html,
+body {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    height: 100%;
+    font-size: 14px;
+}
+body {
+    font-family: "Lucida Grande",Verdana,Arial,"Bitstream Vera Sans",sans-serif;
+    text-align: center;
+    overflow-x: auto;
+    overflow-y: scroll;
+    background: #DDD;
+}
+#wrapper {
+    margin: 0;
+    padding: 1em;
+}
+#main {
+    text-align: left;
+    width: 600px;
+    margin: 0 auto;
+    border: 1px solid #888;
+    background: #FFF;
+    padding: 1em 1em 2em;
+}
+.spacer {
+    clear: both;
+}
+a img {
+    border: none;
+}
+pre {
+    border: 1px solid #888;
+    border-radius: 0.3em;
+    padding: 1em;
+    background: #CCC;
+    overflow-x: auto;
+    overflow-y: auto;
+    resize: vertical;
+}
+h1,
+h2,
+h3,
+h4 {
+    text-shadow: 0 1px 0 #FFFFFF, 0 1px 3px rgba(50, 50, 50, 0.5);
+}
+h1 {
+    font-size: 32px;
+}
+h2 {
+    font-size: 20px;
+    position: absolute;
+    margin: 37px 0 0 290px;
+    font-weight: normal;
+}
+h3 {
+    font-size: 18px;
+}
+h4 {
+    font-size: 16px;
+}
+h2,
+a {
+    color: #777;
+}
+h2 a {
+    display: inline-block;
+    width: 100%;
+    font-size: 13px;
+    text-align: right;
+}
+th {
+    text-align: left;
+    font-size: 13px;
+    text-shadow: 0 1px 0 #FFFFFF, 0 1px 3px rgba(50, 50, 50, 0.5);
+}
+fieldset {
+    background: #EEE;
+    margin-top: 1em;
+}
+legend {
+    padding: 0.3em 1em;
+    background-color: #999;
+    background: -moz-linear-gradient(top,  #CCC,  #999);
+    background: -webkit-linear-gradient(top,  #CCC,  #999);
+    box-shadow: 1px 3px 4px rgba(64, 64, 64, 0.3);
+    color: #FFF;
+    font-size: 10px;
+    font-weight: bold;
+    border: 1px solid #555;
+    text-shadow: 0 1px 1px #000;
+    border-radius: 1em;
+}
+    </style>
 </head>
 <body>
     <div id="wrapper">
